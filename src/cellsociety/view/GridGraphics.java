4aadc5bb07cb305044bcc7af839aa5b64c6dbe96
Harrison Huang <harrison.huang@duke.edu>
2021-02-12 00:30:28 -0500

    changed GridPane implementation to SimulationScreen

--- src/cellsociety/view/GridGraphics.java
+++ src/cellsociety/view/GridGraphics.java
@@ -3,9 +3,7 @@ package cellsociety.view;
 import cellsociety.model.cell.State;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -19,11 +17,11 @@ public class GridGraphics {
   /**
    * Default constructor
    */
-  public GridGraphics(Stage stage) {
-    initialize(stage);
+  public GridGraphics() {
+    initialize();
   }
 
-  private void initialize(Stage stage) {
+  private void initialize() {
     gridPane = new GridPane();
 
 //        Button button1 = new Button("Button 1");
@@ -32,7 +30,7 @@ public class GridGraphics {
 //        gridPane.add(button1, 0, 2);
 //        gridPane.add(button2, 0, 4);
 
-    gridPane.setMinSize(800, 600);
+    //gridPane.setMinSize(800, 600);
 
     //Setting the padding
     gridPane.setPadding(new Insets(10, 10, 10, 10));
@@ -44,14 +42,17 @@ public class GridGraphics {
     //Setting the Grid alignment
     gridPane.setAlignment(Pos.CENTER_RIGHT);
 
-    //Creating a scene object
-    Scene scene = new Scene(gridPane);
-
-    //Setting title to the Stage
-    stage.setTitle("Cell Society Simulation");
-
-    //Adding scene to the stage
-    stage.setScene(scene);
+//    //Creating a scene object
+//    Scene scene = new Scene(gridPane);
+//
+//    //Setting title to the Stage
+//    stage.setTitle("Cell Society Simulation");
+//
+//    //Adding scene to the stage
+//    stage.setScene(scene);
+  }
+  public GridPane getGridPane() {
+    return gridPane;
   }
 
   public void update(State[][] states) {
