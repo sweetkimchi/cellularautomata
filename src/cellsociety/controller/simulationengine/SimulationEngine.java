e18695e56f3ba03d7b2cbd484e86baced43c92bf
Ji Yun Hyo <jiyun.hyo@duke.edu>
2021-02-12 17:30:26 +0000

    added Cornell template for GameofLife

--- src/cellsociety/controller/simulationengine/SimulationEngine.java
+++ src/cellsociety/controller/simulationengine/SimulationEngine.java
@@ -20,21 +20,21 @@ import javafx.stage.Stage;
  */
 public class SimulationEngine extends Simulator {
 
-  private final ArrayList<State> LIST_NAME_BLOCK = new ArrayList<>(
-      Arrays.asList(new State(0, 0, true), new State(0, 1, true), new State(1, 0, true),
-          new State(1, 1, true)));
-  private final ArrayList<State> LIST_NAME_BLINKER = new ArrayList<>(
-      Arrays.asList(new State(0, 0, true), new State(0, 1, true), new State(0, 2, true)));
-  private final ArrayList<State> LIST_NAME_CORNELL = new ArrayList<>(Arrays
-      .asList(new State(1, 0, true), new State(0, 1, true), new State(1, 1, true),
-          new State(2, 1, true)));
-  private final ArrayList<State> LIST_NAME_JIYUN = new ArrayList<>(Arrays
-      .asList(new State(1, 0, true), new State(2, 0, true), new State(0, 1, true),
-          new State(1, 2, true), new State(4, 2, true), new State(2, 3, true),
-          new State(3, 3, true), new State(4, 3, true)));
-  private final ArrayList<State> LIST_NAME_TOAD = new ArrayList<>(Arrays
-      .asList(new State(0, 1, true), new State(0, 2, true), new State(1, 3, true),
-          new State(2, 0, true), new State(3, 1, true), new State(3, 2, true)));
+//  private final ArrayList<State> LIST_NAME_BLOCK = new ArrayList<>(
+//      Arrays.asList(new State(0, 0, true), new State(0, 1, true), new State(1, 0, true),
+//          new State(1, 1, true)));
+//  private final ArrayList<State> LIST_NAME_BLINKER = new ArrayList<>(
+//      Arrays.asList(new State(0, 0, true), new State(0, 1, true), new State(0, 2, true)));
+//  private final ArrayList<State> LIST_NAME_CORNELL = new ArrayList<>(Arrays
+//      .asList(new State(1, 0, true), new State(0, 1, true), new State(1, 1, true),
+//          new State(2, 1, true)));
+//  private final ArrayList<State> LIST_NAME_JIYUN = new ArrayList<>(Arrays
+//      .asList(new State(1, 0, true), new State(2, 0, true), new State(0, 1, true),
+//          new State(1, 2, true), new State(4, 2, true), new State(2, 3, true),
+//          new State(3, 3, true), new State(4, 3, true)));
+//  private final ArrayList<State> LIST_NAME_TOAD = new ArrayList<>(Arrays
+//      .asList(new State(0, 1, true), new State(0, 2, true), new State(1, 3, true),
+//          new State(2, 0, true), new State(3, 1, true), new State(3, 2, true)));
 
 
 
