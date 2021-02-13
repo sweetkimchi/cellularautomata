ba83f45e422471ba33893425fc219d52361e39e8
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-13 18:51:28 -0500

    updating xml files

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -16,6 +16,7 @@ public class Decoder {
   public static final String NUM_ROWS = "numRows";
   public static final String NUM_COLS = "numCols";
 
+  public static final String MODEL = "model";
   private GOLDecoder golDecoder;
   private WaTorDecoder waTorDecoder;
   private SegDecoder segDecoder;
@@ -23,7 +24,6 @@ public class Decoder {
   private PercDecoder percDecoder;
   private String myDesc;
   private String myModel;
-  private String myType;
   private String myTitle;
   private String myAuthor;
   private int myRows;
@@ -75,9 +75,6 @@ public class Decoder {
   }
   public String getAuthor() {
     return myAuthor;
-  }
-  public String getDesc(){
-    return myDesc;
   }
   public int getRows() {
     return myRows;
