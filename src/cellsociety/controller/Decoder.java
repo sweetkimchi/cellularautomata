941d621f1754e3149de11d88b0be48f251a13c08
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-13 17:16:42 -0500

    Added GOLDecoder.java and WaTorDecoder.java, moved getters from Decoder to each class, implemented functionality for checking if model is gameoflife. Need to implement wator model next

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -66,9 +66,6 @@ public class Decoder {
   public String getAuthor() {
     return myAuthor;
   }
-  public String getDesc(){
-    return myDesc;
-  }
   public int getRows() {
     return myRows;
   }
