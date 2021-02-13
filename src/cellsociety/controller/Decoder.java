8009a3a22fa9413644bbae1c69e307026359e1ab
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-13 18:51:28 -0500

    updating xml files

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -23,7 +23,6 @@ public class Decoder {
   private PercDecoder percDecoder;
   private String myDesc;
   private String myModel;
-  private String myType;
   private String myTitle;
   private String myAuthor;
   private int myRows;
