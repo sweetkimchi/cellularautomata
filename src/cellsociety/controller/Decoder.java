b79c6c72dc2b97286084d5591ae934819935248e
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-13 18:54:11 -0500

    updating xml files

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -59,10 +59,12 @@ public class Decoder {
   public String getAuthor() {
     return myAuthor;
   }
+  public String getDesc(){
+    return myDesc;
+  }
   public int getRows() {
     return myRows;
   }
-
   public int getCols() {
     return myCols;
   }
