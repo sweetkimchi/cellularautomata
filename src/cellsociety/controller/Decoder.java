ef1ad45649e80f8369ee3770db9108a5e6a1874e
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-12 20:54:39 -0500

    Added waTor xml file, added initializers for waTor, added getters for waTor parameters, changed "type" in gameOfLife to reflect initial states, changed old "type" to "model"

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -21,7 +21,6 @@ public class Decoder {
   private SegDecoder segDecoder;
   private String myDesc;
   private String myModel;
-  private String myType;
   private String myTitle;
   private String myAuthor;
   private int myRows;
