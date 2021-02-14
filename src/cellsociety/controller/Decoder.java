638a04851629d2115a029bb3a906bcabf8f78193
Shaw Phillips <shaw.phillips@duke.edu>
2021-02-13 19:07:01 -0500

    rebase

--- src/cellsociety/controller/Decoder.java
+++ src/cellsociety/controller/Decoder.java
@@ -16,7 +16,6 @@ public class Decoder {
   public static final String NUM_ROWS = "numRows";
   public static final String NUM_COLS = "numCols";
 
-  public static final String MODEL = "model";
   private GOLDecoder golDecoder;
   private WaTorDecoder waTorDecoder;
   private String myDesc;
