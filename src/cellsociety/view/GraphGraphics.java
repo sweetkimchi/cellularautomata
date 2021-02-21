e264f3256e08a6a8a7298696092516be2a4e0d5c
Harrison Huang <harrison.huang@duke.edu>
2021-02-21 16:26:42 -0500

    refactored GraphGraphics and added javadoc

--- src/cellsociety/view/GraphGraphics.java
+++ src/cellsociety/view/GraphGraphics.java
@@ -1,37 +1,55 @@
 package cellsociety.view;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Data;
-import javafx.scene.chart.XYChart.Series;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * Displays a graph for all cell types in a separate window, runs parallel to the simulation grid
+ * screen.
+ *
+ * @author Harrison Huang
+ */
 public class GraphGraphics {
 
   BorderPane pane;
   Stage stage;
   private int currentTime;
-  private XYChart.Series<String, Number> series;
-  private final LineChart<String, Number> lineChart;
-  private static final int WINDOW_SIZE = 150;
+  private LineChart<String, Number> lineChart;
+  private static final int LINE_WINDOW_SIZE = 150;
+  private static final double WINDOW_WIDTH = 500;
+  private static final double WINDOW_HEIGHT = 500;
   private Map<String,XYChart.Series<String,Number>> lines;
+  private static final String EMPTY_CELL = "empty";
 
+  /**
+   * Creates a new window for displaying the graph.
+   *
+   * @param title title for the graph displayed for the window and at the top
+   * @param xLabel label for the x-axis
+   * @param yLabel label for the y-axis
+   */
   public GraphGraphics(String title, String xLabel, String yLabel) {
+    currentTime = 0;
+    lines = new HashMap<>();
 
     pane = new BorderPane();
     stage = new Stage();
     stage.setTitle(title);
-    currentTime = 0;
+    lineChart = createChart(title, xLabel, yLabel);
+    pane.setCenter(lineChart);
+    Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+    stage.setScene(scene);
+    stage.show();
+  }
 
+  private LineChart<String, Number> createChart(String title, String xLabel, String yLabel) {
     final CategoryAxis xAxis = new CategoryAxis();
     final NumberAxis yAxis = new NumberAxis();
     xAxis.setLabel(xLabel);
@@ -43,32 +61,48 @@ public class GraphGraphics {
     lineChart.setTitle(title);
     lineChart.setAnimated(false);
 
-    lines = new HashMap<>();
-
-    pane.setCenter(lineChart);
-    Scene scene = new Scene(pane, 800, 600);
-    stage.setScene(scene);
-    stage.show();
+    return lineChart;
   }
 
+  /**
+   * Updates the graph given a map of new values. Adds new lines if they do not exist.
+   * Skips all empty cells. Maintains a window size to show a limited time frame.
+   *
+   * @param values a Map of values to update the grid.
+   */
   public void update(Map<String,Integer> values) {
     if (values.keySet().size()!= 0) {
       currentTime++;
       for (String type : values.keySet()) {
-        if (!lines.keySet().contains(type)) {
-          lines.put(type,new XYChart.Series<>());
-          lines.get(type).setName(type);
-          lineChart.getData().add(lines.get(type));
-        }
-        lines.get(type).getData().add(new XYChart.Data<>(""+currentTime,values.get(type)));
-        if (lines.get(type).getData().size() > WINDOW_SIZE) {
-          lines.get(type).getData().remove(0);
-        }
+        if (type.equals(EMPTY_CELL)) continue;
+        putIfAbsent(type);
+        updateLine(type,values.get(type));
       }
     }
   }
+
+  private void putIfAbsent(String type) {
+    if (!lines.keySet().contains(type)) {
+      lines.put(type,new XYChart.Series<>());
+      lines.get(type).setName(type);
+      lineChart.getData().add(lines.get(type));
+    }
+  }
+
+  private void updateLine(String type, Integer value) {
+    lines.get(type).getData().add(new XYChart.Data<>(""+currentTime, value));
+    if (lines.get(type).getData().size() > LINE_WINDOW_SIZE) {
+      lines.get(type).getData().remove(0);
+    }
+  }
+
+  /**
+   * Resets the window and previous data.
+   */
   public void reset() {
-    series.getData().clear();
+    for (XYChart.Series<String,Number> line : lines.values()) {
+      line.getData().clear();
+    }
     lines.clear();
     currentTime = 0;
   }
